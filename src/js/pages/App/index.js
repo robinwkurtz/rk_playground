a54e8f3b6c3f6c2086737a75cd8e5cb660fb54a3
Robin Kurtz <robinwkurtz@gmail.com>
2017-04-15 16:34:08 -0400

    Removing i18n

--- src/js/pages/App/index.js
+++ src/js/pages/App/index.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 import { asyncConnect } from 'redux-connect';
-import { injectIntl, intlShape, IntlProvider } from 'react-intl';
 import classNames from 'classnames';
 
 import config from 'config';
 import Loading from 'loading';
 import Header from 'header';
-import LanguageSwitcher from 'languageSwitcher';
 
 import { load as loadMenu } from 'menu';
 import { loadSingle } from 'pages';
@@ -16,24 +14,18 @@ import { loadSingle } from 'pages';
 import style from './index.scss';
 import gstyles from 'gstyles';
 
-
-/*
-This component wraps the app in a react-intl provider
-It also reacts to changes in the current language and reconfigures the IntlProvider appropriately
-Finally, it displays the Loading bar, the base Helmet config for <head> and the router children
-*/
 class App extends Component {
 	render() {
-		const { i18n, children, menu, page } = this.props;
+		const { children, menu, page } = this.props;
 		console.log('App', this.props);
 		return (
 			<IntlProvider locale={ i18n.language } messages={ i18n.messages }>
 				<main className={ classNames((menu.open) ? [style.main, style.isactive] : style.main) }>
 					<Loading />
-					<Helmet { ...config.app.head[i18n.language] } />
+					<Helmet { ...config.app.head } />
 					<Header { ...this.props } />
 					<div className={ style.body }>
-						{ children }
+						{ React.cloneElement(children, { page }) }
 					</div>
 				</main>
 			</IntlProvider>
@@ -42,7 +34,7 @@ class App extends Component {
 }
 
 const mapStateToProps = state => {
-	return { i18n: state.i18n, menu: state.menu, page: state.pages.page }
+	return { menu: state.menu, page: state.pages.page }
 };
 
 export default asyncConnect([
