e15bf8264146a14de0af6806aa8e4c045f97a19d
Robin Kurtz <robinwkurtz@gmail.com>
2017-04-15 16:23:23 -0400

    Setting up Home component

--- src/js/pages/HomePage/index.js
+++ src/js/pages/HomePage/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { asyncConnect } from 'redux-connect';
+import Helmet from 'react-helmet';
+
+import { loadSingle } from 'pages';
 
 export default class HomePage extends Component {
 	render() {
+		const { page, location } = this.props;
+		console.log('HomePage', this.props);
+		// const path = location.pathname.replace(/^\/|\/$/g, '');
+		// const p = page[(path) ? path : 'home'];
+		// const title = p.title.rendered;
+		// const content = { __html: p.content.rendered };
 		return (
 			<div>
-				<h1>Home</h1>
+				Home
 			</div>
 		);
 	}
