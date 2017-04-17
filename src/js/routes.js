aeb712c5eb2a4788c5c8123abfd82e8a8ef515f0
Robin Kurtz <robinwkurtz@gmail.com>
2017-04-17 19:57:56 -0400

    Adding LostPage for catch all + closeMenu onChange of parent Route

--- src/js/routes.js
+++ src/js/routes.js
@@ -4,9 +4,10 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 import App from './pages/App/';
 import HomePage from './pages/HomePage/';
 import GeneralPage from './pages/GeneralPage/';
+import LostPage from './pages/LostPage/';
 
 import { switchLanguage } from 'i18n';
-import { load as loadMenu } from 'menu';
+import { closeMenu } from 'menu';
 
 /*
 Instead of directly defining our app routes, we have to export a function that receives the store.
@@ -17,15 +18,18 @@ like the best place to do it, specifically in the onEnter hook.
 
 export default function createRoutes(store) {
     return (
-        <Route path="/" component={App}>
-            <Route path="fr" onEnter={() => store.dispatch(switchLanguage('fr'))}>
-                <IndexRoute component={HomePage} />
-            </Route>
+        <Route
+            path="/"
+            component={App}
+            onChange={() => store.dispatch(closeMenu())}
+        >
             <Route onEnter={() => store.dispatch(switchLanguage('en'))}>
                 <IndexRoute component={HomePage} />
                 <Route path="about-me" component={GeneralPage} />
                 <Route path="what-i-do" component={GeneralPage} />
-                <Route path="contact-me" component={GeneralPage} />
+                <Route path="curriculum-vitae" component={GeneralPage} />
+                <Route path="say-hello" component={GeneralPage} />
+                <Route path="*" component={LostPage} />
             </Route>
         </Route>
     );
