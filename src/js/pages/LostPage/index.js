cb23e72612b4a46894bc36a1100457112d71520d
Robin Kurtz <robinwkurtz@gmail.com>
2017-04-17 20:02:12 -0400

    Creating LostPage component

---
+++ src/js/pages/LostPage/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { asyncConnect } from 'redux-connect';
+
+import { loadSingle } from 'pages';
+
+class LostPage extends Component {
+	render() {
+		const { page } = this.props;
+		const p = this.props.page.lost;
+		const content = { __html: p.content.rendered };
+		return (
+			<div className='inner'>
+				<div dangerouslySetInnerHTML={content} />
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return { menu: state.menu, page: state.pages.page }
+};
+
+export default asyncConnect([
+	{
+		deferred: true,
+		promise: ({ store: { dispatch } }) => {
+			return dispatch(loadSingle('lost'));
+		}
+	}
+])(
+	connect(
+		mapStateToProps, null
+	)(LostPage)
+);
