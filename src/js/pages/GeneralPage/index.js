38317fb3675a3048eea91210828cc93ac3bfdc2b
Robin Kurtz <robinwkurtz@gmail.com>
2017-04-17 20:01:53 -0400

    Adding page content, removing title

--- src/js/pages/GeneralPage/index.js
+++ src/js/pages/GeneralPage/index.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 export default class GeneralPage extends Component {
 	render() {
+		const { page, location } = this.props;
+		const path = location.pathname.replace(/^\/|\/$/g, '');
+		const p = page[path];
+		const title = p.title.rendered;
+		const content = { __html: p.content.rendered };
 		return (
 			<div>
-				<h1>GeneralPage</h1>
+				<div dangerouslySetInnerHTML={content} />
 			</div>
 		);
 	}
--- src/js/pages/HomePage/index.js
+++ src/js/pages/HomePage/index.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { asyncConnect } from 'redux-connect';
-import Helmet from 'react-helmet';
-
-import { loadSingle } from 'pages';
 
 export default class HomePage extends Component {
 	render() {
-		const { page, location } = this.props;
-		console.log('HomePage', this.props);
-		// const path = location.pathname.replace(/^\/|\/$/g, '');
-		// const p = page[(path) ? path : 'home'];
-		// const title = p.title.rendered;
-		// const content = { __html: p.content.rendered };
+		const p = this.props.page.home;
+		const content = { __html: p.content.rendered };
 		return (
-			<div>
-				Home
+			<div className="row">
+				<div
+					className="column"
+					dangerouslySetInnerHTML={content}
+				/>
 			</div>
 		);
 	}
