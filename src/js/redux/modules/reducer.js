8bdf3c194cfdc9ba64f20614a13bffc4e871cf5d
Robin Kurtz <robinwkurtz@gmail.com>
2017-04-23 20:15:28 -0400

    Replacing various paths to Webpack aliases

--- src/js/redux/modules/reducer.js
+++ src/js/redux/modules/reducer.js
@@ -3,9 +3,10 @@ import { routerReducer } from 'react-router-redux';
 import { reducer as reduxAsyncConnect } from 'redux-connect';
 import { reducer as uiReducer } from 'redux-ui'
 
-import ssrReducer from './ssr';
-import pagesReducer from './pages';
-import menuReducer from './menu';
+import ssrReducer from 'ssr';
+import pagesReducer from 'pages';
+import menuReducer from 'menu';
+import siteInformationReducer from 'site';
 
 /*
   This is our main app reducer.
@@ -16,6 +17,7 @@ export default combineReducers({
     routing: routerReducer,
     reduxAsyncConnect,
     ui: uiReducer,
+    site: siteInformationReducer,
     pages: pagesReducer,
     menu: menuReducer,
     ssr: ssrReducer
