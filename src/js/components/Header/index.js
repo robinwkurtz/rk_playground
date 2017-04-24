7b4eb8faa114a604ddf782fd3694b820807cf2fe
Robin Kurtz <robinwkurtz@gmail.com>
2017-04-23 20:12:24 -0400

    Big commit: Adding legal from API + Dynamic year to replace API content shortcode, Adding titles to links + adding markup for legal

--- src/js/components/Header/index.js
+++ src/js/components/Header/index.js
@@ -25,12 +25,21 @@ export default class Header extends Component {
 		)
 	}
 	render() {
-		const { dispatch, menu } = this.props
+		const { dispatch, menu, site } = this.props
+
+		const date = new Date();
+		const year = date.getFullYear();
+		let legal = site.data.site_information_legal[0].replace('[year]', year);
+		legal = { __html: legal };
 		return (
 			<header className={ style.header }>
 	            <div className="row full flush">
 	                <div className="column float">
-	                    <Link className={ style.logo } to="/">
+	                    <Link
+							className={ style.logo }
+							to="/"
+							title="Home"
+						>
 	                        <div className="show-for-medium-up">
 	                            { config.app.logo.full }
 	                        </div>
@@ -44,6 +53,7 @@ export default class Header extends Component {
 							href="#"
 							onClick={ (e) => { this.clickToggleMenu(e, dispatch) } }
 							className={ classNames(style.menubutton, (menu.open) ? style.isactive : '' ) }
+							title="Navigation"
 						>
 	                        <div className={ style.burger }>
 	                            <div className={ style.burgericon }>Menu</div>
@@ -55,7 +65,7 @@ export default class Header extends Component {
 	            <div className={ classNames(style.menu, (menu.open) ? style.isactive : '' ) }>
 					<Navigation { ...this.props } />
 	                <div className={ style.copyright }>
-	                    Copyright
+						<p dangerouslySetInnerHTML={legal} />
 	                </div>
 	            </div>
         	</header>
