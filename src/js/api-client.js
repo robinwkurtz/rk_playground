7780a5ade675ad6090d186c0d94b30ca608594e0
Robin Kurtz <robinwkurtz@gmail.com>
2017-04-23 20:08:46 -0400

    Removing API_URL_MENUS as it’s now combined within API_URL + Adding fetchSiteInfo()

--- src/js/api-client.js
+++ src/js/api-client.js
@@ -1,36 +1,35 @@
-/*
-This module is used to make API calls to WordPress.
-It uses the isomorphic-fetch library which exports a fetch function.
-In essence, fetch takes a URL and returns a promise for the respone.
-It's the little brother of XMLHttpRequest, and it works on the server too.
-*/
-
 import fetch from 'isomorphic-fetch';
 import querystring from 'querystring';
 
-import { API_URL, API_URL_MENUS } from 'config';
+import { API_URL } from 'config';
 
 export function fetchMenus() {
-	return fetch(API_URL_MENUS)
-	.then(res => res.json());
+	return fetch(`${API_URL}wp-api-menus/v2/menus`)
+	.then(r => r.json());
 }
 
 export function fetchMenu(id) {
-	return fetch(`${API_URL_MENUS}/${id}`)
-	.then(res => res.json());
+	return fetch(`${API_URL}wp-api-menus/v2/menus/${id}`)
+	.then(r => r.json());
 }
 
 export function fetchPages(language) {
-	return fetch(`${API_URL}page`)
-	.then(res => res.json())
-	.then(res => res[0]);
+	return fetch(`${API_URL}wp/v2/pages`)
+	.then(r => r.json())
+	.then(r => r[0]);
 }
 
 export function fetchPage(slug) {
 	const pathname = (slug) ? slug : 'home';
 	return fetch(
-		`${API_URL}pages?${querystring.stringify({ slug: pathname })}`
+		`${API_URL}wp/v2/pages?${querystring.stringify({ slug: pathname })}`
 	)
-	.then(res => res.json())
-	.then(res => res[0]);
+	.then(r => r.json())
+	.then(r => r[0]);
+}
+
+export function fetchSiteInfo() {
+	return fetch(`${API_URL}siteinfo/v1/content`)
+	.then(r => r.json())
+	.then(r => r[0]);
 }
