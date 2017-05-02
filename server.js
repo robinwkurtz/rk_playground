e6afa650eec65502115106b190339602c70d1f66
Robin Kurtz <robinwkurtz@gmail.com>
2017-05-01 20:49:58 -0400

    Adjusting port to 5000

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ export default function server(parameters) {
   });
 
   const server = require('http').createServer(app);
-  const PORT = process.env.PORT || 4200;
+  const PORT = process.env.PORT || 5000;
 
   server.listen(PORT, process.env.IP || '0.0.0.0', function(err) {
     if (err) {
