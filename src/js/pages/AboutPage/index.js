3fd1195d241d35c99c3ac9757a26892217e912a1
Robin Kurtz <robinwkurtz@gmail.com>
2017-05-04 20:42:38 -0400

    Adding AboutPage + styles

---
+++ src/js/pages/AboutPage/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import style from './index.scss';
+
+export default class AboutPage extends Component {
+	render() {
+		const { page, location } = this.props;
+		const path = location.pathname.replace(/^\/|\/$/g, '');
+		const p = page[path];
+		const title = p.title.rendered;
+		const content = { __html: p.content.rendered };
+		return (
+			<div className="row">
+				<div className="column small-12 medium-4">
+					<div className={style.bioimage}>
+						<div>
+							<div className="square" />
+						</div>
+					</div>
+					<br />
+					{ /* TO-DO: Make cooler, make it react more! */ }
+					<div className="css-bounce align-center show-for-medium-up">
+						<h4>
+							click & hold me ;)
+						</h4>
+					</div>
+				</div>
+				<div className="column small-12 show-for-small-only" />
+				<div
+					className="column small-12 medium-7 large-6 large-offset-1 content"
+					dangerouslySetInnerHTML={content}
+				/>
+			</div>
+		);
+	}
+}
